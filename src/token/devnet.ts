c8c92fc863de70db333677c9969991f8fa2c28e6
Dan Hsu <hsu86110@gmail.com>
2022-06-21 00:09:13 +0800

    feature: commends DEVNET_LP_TOKENS

--- src/token/devnet.ts
+++ src/token/devnet.ts
@@ -1,8 +1,8 @@
 import { LpTokens} from "./type";
 
 
-export const DEVNET_LP_TOKENS: LpTokens = {
-  *[Symbol.iterator]() {
-    yield* Object.values(this);
-  },
-};
+// export const DEVNET_LP_TOKENS: LpTokens = {
+//   *[Symbol.iterator]() {
+//     yield* Object.values(this);
+//   },
+// };
